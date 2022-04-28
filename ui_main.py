c753fb707532a45d24780ebd478086b7115545e4
Administrator <11>
2022-04-28 16:29:23 +0900

    done

---
+++ ui_main.py
@@ -0,0 +1,26 @@
+from PySide2 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(798, 660)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.webview = QWebEngineView(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(8)
+        sizePolicy.setHeightForWidth(self.webview.sizePolicy().hasHeightForWidth())
+        self.webview.setSizePolicy(sizePolicy)
+        self.webview.setObjectName("webview")
+        self.verticalLayout.addWidget(self.webview)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+
+from PySide2.QtWebEngineWidgets import QWebEngineView
